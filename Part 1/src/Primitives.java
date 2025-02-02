dae7f2965aa6b0a997c3fc7d88011af891e3c57e
Andrius Žimkus <and.zimkus@gmail.com>
2025-02-02 20:45:48 +0200

    2025-02-02 20:45:48

--- Part 1/src/Primitives.java
+++ Part 1/src/Primitives.java
@@ -1,7 +1,17 @@
+import java.util.Arrays;
+
 public class Primitives {
     public static void main(String[] args) {
-        int a = 3;
-        System.out.println(a);
+        int b = 3;
+        System.out.println(b);
+
+        int N = 5;
+        double[] a;
+        a = new double[N];
+        for (int i = 0; i < N; i++)
+            a[i] = 0.0;
+
+        System.out.println(Arrays.toString(a));
 
     }
 }
